f18b9dac4db964449ef6d38d08b653264dc5ee72
solux <lee.yeji0327@gmail.com>
2024-08-29 00:53:17 +0900

    [feat] togglebtn 생성

---
+++ src/components/ToggleBtn.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { useTheme } from "../contexts/ThemeContext";
+
+const ToggleBtn = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <ToggleButton onClick={toggleTheme}>
+      {theme === "light" ? " light 🌝" : "dark 🌚"}
+    </ToggleButton>
+  );
+};
+
+export default ToggleBtn;
+
+const ToggleButton = styled.button`
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  padding: 20px 40px;
+  font-size: 20px;
+  border-radius: 50px;
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.textColor};
+  border: ${(props) => props.borderColor};
+`;
