2e81f7384ac65204da5ae1923cac9358916579a7
solux <lee.yeji0327@gmail.com>
2024-08-28 23:50:02 +0900

    [add] reviewpage context 사용

--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReviewList from "../components/ReviewList";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
+import ThemeContext from "../contexts/ThemeContext";
 
 const ReviewPage = () => {
   const navigate = useNavigate();
+  const theme = useContext(ThemeContext);
 
   return (
     <>
       <Wrapper>
-        <h1>테마 : light</h1>
+        <h1>테마 : {theme}</h1>
 
         <button onClick={() => navigate("/create")}>작성하기</button>
         <ReviewList />
