6ecc996f93ae782bb1d7c04c5e02f14f8aee0e1d
solux <lee.yeji0327@gmail.com>
2024-08-29 00:51:15 +0900

    [feat] themecontext provider, 커스텀훅

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,5 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
 
-export default ThemeContext;
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    console.log("테마 변경: ", theme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
